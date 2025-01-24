2797525abf26034cb89f41833cea3e7e2d0963aa
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-24 15:13:53 +0600

    Modify protected route

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -4,10 +4,10 @@ import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 import { useRouter } from "next/navigation";
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, isLoading } = useKindeAuth();
+  const { isAuthenticated } = useKindeAuth();
   const router = useRouter();
 
-  if (isLoading) return <div>Loading...</div>;
+
   if (!isAuthenticated) {
     router.push("/api/auth/login");
     return null;
