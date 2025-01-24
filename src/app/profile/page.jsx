8725a896893d458c895d3366431d95a152ebe30b
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-24 11:01:19 +0600

    Modify profile page again

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -22,10 +22,6 @@ const Profile = () => {
     </div>;
   }
 
-  if (!isAuthenticated) {
-    return null;
-  }
-
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold text-purple-900">
