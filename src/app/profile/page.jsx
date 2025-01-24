3340e22b87181dc1d8678bd01b0fa21cb57bcf76
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-24 16:22:18 +0600

    Modify profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,26 +1,26 @@
 "use client";
 
-import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
-import { useRouter } from "next/navigation";
-import Image from "next/image";
-import loadingImage from "../../../public/loading.gif";
+// import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+// import { useRouter } from "next/navigation";
+// import Image from "next/image";
+// import loadingImage from "../../../public/loading.gif";
 
 const Profile = () => {
-  const { isAuthenticated, isLoading } = useKindeAuth();
-  const router = useRouter();
+  // const { isAuthenticated, isLoading } = useKindeAuth();
+  // const router = useRouter();
 
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <Image src={loadingImage} alt="Loading..." width={50} height={50} />
-      </div>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <div className="flex items-center justify-center min-h-screen">
+  //       <Image src={loadingImage} alt="Loading..." width={50} height={50} />
+  //     </div>
+  //   );
+  // }
 
-  if (!isAuthenticated) {
-    router.push("/api/auth/login");
-    return null;
-  }
+  // if (!isAuthenticated) {
+  //   router.push("/api/auth/login");
+  //   return null;
+  // }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
