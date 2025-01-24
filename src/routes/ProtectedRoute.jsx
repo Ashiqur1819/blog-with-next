7e7a9c00678fbb73097136a93704e0ea0fba2f0e
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-24 15:20:31 +0600

    Modify loader

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -2,15 +2,19 @@
 
 import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
 import { useRouter } from "next/navigation";
-import loadingImage from "../../public/loading.gif"
+import loadingImage from "../../public/loading.gif";
+import Image from "next/image";
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated, isLoading } = useKindeAuth();
   const router = useRouter();
 
-  if (isLoading) return <div className="flex items-center justify-center min-h-screen">
-          <Image src={loadingImage} alt="Loading..." width={50} height={50} />
-        </div>
+  if (isLoading)
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Image src={loadingImage} alt="Loading..." width={50} height={50} />
+      </div>
+    );
   if (!isAuthenticated) {
     router.push("/api/auth/login");
     return null;
