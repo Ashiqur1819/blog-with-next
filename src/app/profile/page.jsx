29fd424fec913c6759721c5bd3153a250afaee3a
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-24 19:27:38 +0600

    Modify profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,41 +1,37 @@
+"use client";
+
+import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import loadingImage from "../../../public/loading.gif";
+
+const Profile = () => {
+  const { isAuthenticated, isLoading } = useKindeAuth();
+  const router = useRouter();
 
 
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+  if (isLoading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <Image src={loadingImage} alt="Loading..." width={50} height={50} />
+      </div>
+    );
+  }
 
 
+  if (!isAuthenticated) {
+    router.push("/api/auth/login");
+    return null;
+  }
 
-// import { useKindeAuth } from "@kinde-oss/kinde-auth-nextjs";
-// import { useRouter } from "next/navigation";
-// import Image from "next/image";
-// import loadingImage from "../../../public/loading.gif";
-import { redirect } from "next/navigation";
 
-const Profile = async() => {
-
-    const {getUser} = getKindeServerSession()
-    const user = await getUser()
-  
-  // const { isAuthenticated, isLoading } = useKindeAuth();
-
-  // if (isLoading) {
-  //   return (
-  //     <div className="flex items-center justify-center min-h-screen">
-  //       <Image src={loadingImage} alt="Loading..." width={50} height={50} />
-  //     </div>
-  //   );
-  // }
-return (
-  user ? (
+  return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold text-purple-900">
         Welcome to your profile!
       </h1>
     </div>
-  ) : (
-    redirect("/api/auth/login")
-  ))
-
-  
+  );
 };
 
 export default Profile;
