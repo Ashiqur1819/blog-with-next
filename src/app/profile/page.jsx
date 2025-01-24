d876c6b51f6d3bad9eebfd22723e9498023990b9
Ashiqur1819 <ashiqurrahman1819@gmail.com>
2025-01-24 15:46:51 +0600

    Modify profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -6,15 +6,10 @@ import loadingImage from "../../../public/loading.gif";
 import Image from "next/image";
 
 const Profile = () => {
-  const { isAuthenticated, isLoading, error } = useKindeBrowserClient();
+  const { isAuthenticated, isLoading} = useKindeBrowserClient();
 
-  // Debugging logs
-  console.log("isLoading:", isLoading);
-  console.log("isAuthenticated:", isAuthenticated);
-  console.log("error:", error);
 
   if (isLoading) {
-    // Show a loading spinner
     return (
       <div className="flex items-center justify-center min-h-screen">
         <Image src={loadingImage} className="w-12" alt="Loading..." />
@@ -22,19 +17,10 @@ const Profile = () => {
     );
   }
 
-  if (error) {
-    return (
-      <div className="flex items-center justify-center min-h-screen">
-        <h1 className="text-red-500">Authentication Error: {error.message}</h1>
-      </div>
-    );
-  }
-
   if (!isAuthenticated) {
-    redirect("/api/auth/login"); // Redirect to login if not authenticated
+    redirect("/api/auth/login"); 
   }
 
-  // Render the profile page if authenticated
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-4xl font-bold text-purple-900">
